7e7756d0b8a55fcf238a1a032cbb99f5038ff8ff
sameermanjrekaragilisium <sameer.manjrekar@agilisium.com>
2023-02-04 12:50:57 +0530

    Added Header and react icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
-import './globals.css'
+import "./globals.css";
+
+import Header from "@/components/Header";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
